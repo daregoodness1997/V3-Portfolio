3bc08bc5d01aa365f3cb4512e1bbd30bb6fdad1e
Dare Goodness <daregoodness@gmail.com>
2023-03-12 12:17:24 +0100

    navigation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { navLinks } from '../constants';
 import { styles } from '../styles';
 
 const Navbar = () => {
+  const [active, setActive] = useState('');
   return (
     <nav
-      className={`${styles.paddingX} w-full flex items-center py-6 fixed top-0 z-20 bg-primary`}
+      className={`${styles.paddingX} w-full flex items-center py-6 fixed top-0 z-20 bg-primary backdrop-blur-md`}
     >
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
-        <Link to='/'>Dare Goodness</Link>
+        <Link
+          to='/'
+          onClick={() => {
+            setActive('');
+            window.scrollTo(0, 0);
+          }}
+          className='flex gap-2 items-center'
+        >
+          <img
+            src='logo.png'
+            alt='Dare Goodness Logo'
+            className='w-9 h-9 object-contain'
+          />
+          Dare Goodness
+        </Link>
+
+        <ul className='list-none hidden sm:flex flex-row gap-10'>
+          {navLinks.map(nav => (
+            <li
+              key={nav.id}
+              className={`${
+                active === nav.title ? 'text-white' : 'text-secondary'
+              } hover:text-white text-[14px] font-medium cursor-pointer`}
+              onClick={() => setActive(nav.title)}
+            >
+              <a href={`#${nav.id}`}>{nav.title}</a>
+            </li>
+          ))}
+        </ul>
       </div>
     </nav>
   );
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,23 @@
-const navLinks = [{}];
+const navLinks = [
+  {
+    id: 'about',
+    title: 'About',
+  },
+  {
+    id: 'work',
+    title: 'Work',
+  },
+  {
+    id: 'not-design',
+    title: 'Not Design',
+  },
+  {
+    id: 'contact',
+    title: 'Contact',
+  },
+];
 
 export { navLinks };
+
+// https://www.phyllosophy.me/seattleworks
+// https://www.rachelyhe.com/
