b94ae72c98fcba952e31ee54b7f1e1db0261aa1a
Dare Goodness <daregoodness@gmail.com>
2023-03-22 20:56:22 +0100

    feat: added smooth scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -10,27 +10,36 @@ import {
   Works,
 } from './components';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { motion, useScroll, useSpring } from 'framer-motion';
 
 const App = () => {
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 10000,
+    damping: 300,
+  });
   return (
-    <Router>
-      <div className='relative z-0 bg-primary'>
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
-          <Navbar />
-          <Hero />
-        </div>
+    <>
+      <motion.div className='progress-bar' style={{ scaleX }} />
+      <Router>
+        <div className='relative z-0 bg-primary'>
+          <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+            <Navbar />
+            <Hero />
+          </div>
 
-        <Tech />
-        <Works />
-        <Experience />
-        <Feedbacks />
-        <div className='relative z-0'>
-          <Contact />
-          <StarsCanvas />
+          <Tech />
+          <Works />
+          <Experience />
+          <Feedbacks />
+          <div className='relative z-0'>
+            <Contact />
+            <StarsCanvas />
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
-    </Router>
+      </Router>
+    </>
   );
 };
 
